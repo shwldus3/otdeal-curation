4332e97a5e50d6be549c40ae680beeeee4ae9141
NohJi yeon <shwldus3@gmail.com>
2015-05-24 19:46:59 +0900

    5분으로 재설

--- batchjob/schedule.js
+++ batchjob/schedule.js
@@ -8,8 +8,13 @@ var curation = require('./curation');
 // [MINUTE] [HOUR] [DAY OF MONTH] [MONTH OF YEAR] [DAY OF WEEK] [YEAR (optional)]
 // 분 시 일 월 요일 년
 // (0-59) (0~23) (1~31) (1~12) (0~7):0(또는7)이 일요일.
+// '* * * * * *' - runs every second
+// '*/5 * * * * *' - runs every 5 seconds
+// '10,20,30 * * * * *' - run at 10th, 20th and 30th second of every minute
+// '0 * * * * *' - runs every minute
+// '0 0 * * * *' - runs every hour (at 0 minutes and 0 seconds)
 
-var cronStyle = '5 * * * *'; // 매 분마다 실행.
+var cronStyle = '*/5 * * * *'; // 매 분마다 실행.
 var j3 = schedule.scheduleJob(cronStyle, function(){
 	logger.info('5 분마다 실행');
 	curation.curationAlgorithm(function(err){
